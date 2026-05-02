a34f3898f4e346d2c0ddbe35b922af881b70d126
alicheikh <cheikhalicheikh593@gmail.com>
2026-05-02 12:16:39 +0200

    fix : add missing validation rules to ReceiveStockCommand

--- stock-application/src/main/java/com/aliCheikh/stock/application/dto/ReceiveStockCommand.java
+++ stock-application/src/main/java/com/aliCheikh/stock/application/dto/ReceiveStockCommand.java
@@ -3,6 +3,7 @@ package com.aliCheikh.stock.application.dto;
 import com.aliCheikh.stock.domain.model.shop.ShopId;
 import com.aliCheikh.stock.domain.model.user.UserId;
 import java.util.List;
+import java.util.Objects;
 
 public record ReceiveStockCommand(
         String productReference,       // On cherche par référence !
@@ -10,4 +11,19 @@ public record ReceiveStockCommand(
         ShopId shopId,// Peut être 'null' si le produit existe déjà
         UserId userId,
         List<TargetLocation> distributions // La répartition demandée
-) {}
+) {
+    public ReceiveStockCommand {
+        Objects.requireNonNull(productReference, "productReference cannot be null");
+        Objects.requireNonNull(distributions, "distributions cannot be null");
+        Objects.requireNonNull(shopId, "shopId cannot be null");
+        Objects.requireNonNull(userId, "userId cannot be null");
+        if(productReference.isBlank()){
+            throw new IllegalArgumentException("productReference cannot be blank");
+        }
+        if (distributions.isEmpty()) {
+            throw new IllegalArgumentException("distributions cannot be empty");
+        }
+
+    }
+
+}
